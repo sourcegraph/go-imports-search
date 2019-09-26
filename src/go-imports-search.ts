7409bf7fc64ee0233b739f3f3da4dc71e2be008a
Farhan Attamimi <farhan@sourcegraph.com>
2019-09-26 11:11:52 -0700

    Turn off see all usages links by default, add goImports.showAllUsagesLinks setting to turn on

--- src/go-imports-search.ts
+++ src/go-imports-search.ts
@@ -1,63 +1,65 @@
-import { EMPTY, from, of } from 'rxjs';
-import { concatMap, toArray } from 'rxjs/operators';
-import * as sourcegraph from 'sourcegraph';
+import { EMPTY, from, of } from 'rxjs'
+import { concatMap, toArray } from 'rxjs/operators'
+import * as sourcegraph from 'sourcegraph'
+import { resolveSettings, Settings } from './settings'
 
 export function activate(): void {
+    const settings = resolveSettings(sourcegraph.configuration.get<Settings>().value)
+
     sourcegraph.search.registerQueryTransformer({
         transformQuery: (query: string) => {
-            const goImportsRegex = /\bgo.imports:([^\s]*)/;
+            const goImportsRegex = /\bgo.imports:([^\s]*)/
             if (query.match(goImportsRegex)) {
                 // Get package name
-                const pkgFilter = query.match(goImportsRegex);
-                const pkg =
-                    pkgFilter && pkgFilter.length >= 1 ? pkgFilter[1] : '';
+                const pkgFilter = query.match(goImportsRegex)
+                const pkg = pkgFilter && pkgFilter.length >= 1 ? pkgFilter[1] : ''
 
                 // Package imported in grouped import statements
-                const matchPackage = '^\\t"[^\\s]*' + pkg + '[^\\s]*"$';
+                const matchPackage = '^\\t"[^\\s]*' + pkg + '[^\\s]*"$'
                 // Match packages with aliases
-                const matchAlias = '\\t[\\w/]*\\s"[^\\s]*' + pkg + '[^\\s]*"$';
+                const matchAlias = '\\t[\\w/]*\\s"[^\\s]*' + pkg + '[^\\s]*"$'
                 // Match packages in single import statement
-                const matchSingle = 'import\\s"[^\\s]*' + pkg + '[^\\s]*"$';
-                const finalRegex = `(${matchPackage}|${matchAlias}|${matchSingle}) lang:go `;
+                const matchSingle = 'import\\s"[^\\s]*' + pkg + '[^\\s]*"$'
+                const finalRegex = `(${matchPackage}|${matchAlias}|${matchSingle}) lang:go `
 
-                return query.replace(goImportsRegex, finalRegex);
+                return query.replace(goImportsRegex, finalRegex)
             }
-            return query;
-        }
-    });
+            return query
+        },
+    })
 
     sourcegraph.workspace.onDidOpenTextDocument.subscribe(doc => {
+        if (!settings['goImports.showAllUsagesLinks']) {
+            return
+        }
+
         if (doc.languageId !== 'go') {
             return
         }
-        from(doc.text.split('\n')).pipe(
-            concatMap(
-                (line, lineNumber) => {
+        from(doc.text.split('\n'))
+            .pipe(
+                concatMap((line, lineNumber) => {
                     // An issue with the second regex is that it'll match lines like
                     //  return "string"
                     const goPkgRegex = /\t\"([^\s]*)\"$|\t([^\s]*)\s"[^\s]*"$|import\s"([^\s]*)"$/
-                    const match = goPkgRegex.exec(line);
+                    const match = goPkgRegex.exec(line)
                     if (match && match.length > 1) {
                         // The match index depends on which regex pattern actually produced a match
                         const pkgName = match[1] ? match[1] : match[2] ? match[2] : match[3]
-                        return of({lineNumber, pkgName});
+                        return of({ lineNumber, pkgName })
                     }
-                    return EMPTY;
+                    return EMPTY
+                }),
+                toArray()
+            )
+            .subscribe(matches => {
+                if (!matches) {
+                    return
                 }
-            ),
-            toArray()
-        ).subscribe(matches => {
-            if (!matches) {
-                return
-            }
-            if (
-                sourcegraph.app.activeWindow &&
-                sourcegraph.app.activeWindow.visibleViewComponents.length >
-                    0
-            ) {
-                sourcegraph.app.activeWindow.visibleViewComponents[0].setDecorations(
-                    null,
-                    matches.map(match => ({
+                if (sourcegraph.app.activeWindow && sourcegraph.app.activeWindow.visibleViewComponents.length > 0) {
+                    sourcegraph.app.activeWindow.visibleViewComponents[0].setDecorations(
+                        null,
+                        matches.map(match => ({
                             range: new sourcegraph.Range(
                                 new sourcegraph.Position(match.lineNumber, 0),
                                 new sourcegraph.Position(match.lineNumber, 0)
@@ -66,13 +68,13 @@ export function activate(): void {
                                 contentText: 'See all usages',
                                 linkURL: '/search?q=go.imports:' + match.pkgName,
                                 backgroundColor: 'pink',
-                                color: 'black'
-                            }
-                    })
-                ))
-            }
-            });
-    });
+                                color: 'black',
+                            },
+                        }))
+                    )
+                }
+            })
+    })
 }
 
 // See https://about.sourcegraph.com/blog/extension-authoring for instructions and examples.
