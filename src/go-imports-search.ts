1dee35992d5a00bc30a291253045495d3f70104c
TJ Kandala <kandalatj@gmail.com>
2021-09-14 21:00:19 -0400

    refactor: single condition early return

--- src/go-imports-search.ts
+++ src/go-imports-search.ts
@@ -55,26 +55,25 @@ export function activate(): void {
                 toArray()
             )
             .subscribe(matches => {
-                if (!matches) {
+                if (!matches || sourcegraph.app.activeWindow?.activeViewComponent?.type !== 'CodeEditor') {
                     return
                 }
-                if (sourcegraph.app.activeWindow?.activeViewComponent?.type === 'CodeEditor') {
-                    sourcegraph.app.activeWindow.activeViewComponent.setDecorations(
-                        goImportsDecorationType,
-                        matches.map(match => ({
-                            range: new sourcegraph.Range(
-                                new sourcegraph.Position(match.lineNumber, 0),
-                                new sourcegraph.Position(match.lineNumber, 0)
-                            ),
-                            after: {
-                                contentText: 'See all usages',
-                                linkURL: '/search?q=go.imports:' + match.pkgName,
-                                backgroundColor: 'pink',
-                                color: 'black',
-                            },
-                        }))
-                    )
-                }
+
+                sourcegraph.app.activeWindow.activeViewComponent.setDecorations(
+                    goImportsDecorationType,
+                    matches.map(match => ({
+                        range: new sourcegraph.Range(
+                            new sourcegraph.Position(match.lineNumber, 0),
+                            new sourcegraph.Position(match.lineNumber, 0)
+                        ),
+                        after: {
+                            contentText: 'See all usages',
+                            linkURL: '/search?q=go.imports:' + match.pkgName,
+                            backgroundColor: 'pink',
+                            color: 'black',
+                        },
+                    }))
+                )
             })
     })
 }
